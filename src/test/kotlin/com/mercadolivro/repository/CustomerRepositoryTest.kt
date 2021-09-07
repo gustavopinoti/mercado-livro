00ffca1aefee0adc6dbc83a6a0669fde04a17e19
daniel <daniel_donato10@yahoo.com.br>
2021-09-07 15:08:49 -0300

    Repository tests

--- src/test/kotlin/com/mercadolivro/repository/CustomerRepositoryTest.kt
+++ src/test/kotlin/com/mercadolivro/repository/CustomerRepositoryTest.kt
@@ -1,10 +1,13 @@
 package com.mercadolivro.repository
 
 import com.mercadolivro.enums.CustomerStatus
+import com.mercadolivro.helpers.buildCustomer
 import com.mercadolivro.model.CustomerModel
 import com.mercadolivro.repository.CustomerRepository
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -24,28 +27,64 @@ class CustomerRepositoryTest {
     @BeforeEach
     fun setUp() = customerRepository.deleteAll()
 
-    @Test
-    fun `should return naming containing with prefix`() {
-        val matheus = createCustomer(name = "matheus")
-        val maria = createCustomer(name = "maria")
-        val alex = createCustomer(name = "alex")
-        val expectedCustomers = listOf(matheus, maria)
+    @Nested
+    inner class FindByNameContaining {
+        @Test
+        fun `should return naming containing with prefix`() {
+            val matheus = customerRepository.save(buildCustomer(name = "matheus"))
+            val maria = customerRepository.save(buildCustomer(name = "maria"))
+            val alex = customerRepository.save(buildCustomer(name = "alex"))
+            val expectedCustomers = listOf(matheus, maria)
 
-        val customers = customerRepository.findByNameContaining("ma")
+            val customers = customerRepository.findByNameContaining("ma")
 
-        Assertions.assertEquals(expectedCustomers, customers)
+            assertEquals(expectedCustomers, customers)
+        }
     }
 
+    @Nested
+    inner class ExistsByEmail() {
+        @Test
+        fun `should return true when has email`() {
+            val email = "customeremail@gmail.com"
+            val customer = customerRepository.save(buildCustomer(email = email))
 
-    private fun createCustomer(
-        name: String = "Customer name",
-        email: String = "${UUID.randomUUID()}@email.com"
-    ) = customerRepository.save(CustomerModel(
-            id = null,
-            name = name,
-            email = email,
-            status = CustomerStatus.ATIVO,
-            password = UUID.randomUUID().toString(),
-            roles = emptySet()
-    ))
+            val result = customerRepository.existsByEmail(email)
+
+            assertTrue(result)
+        }
+
+        @Test
+        fun `should return null when not has email`() {
+            val email = "nonexistentemail@gmail.com"
+
+            val result = customerRepository.existsByEmail(email)
+
+            assertFalse(result)
+        }
+    }
+
+    @Nested
+    inner class FindByEmail() {
+        @Test
+        fun `should return customer when has email`() {
+            val email = "customeremail@gmail.com"
+            val customer = customerRepository.save(buildCustomer(email = email))
+
+            val result = customerRepository.findByEmail(email)
+
+            assertNotNull(result)
+            assertEquals(customer, result)
+        }
+
+        @Test
+        fun `should return null when not has email`() {
+            val email = "nonexistentemail@gmail.com"
+
+            val result = customerRepository.findByEmail(email)
+
+            assertNull(result)
+        }
+    }
+
 }
